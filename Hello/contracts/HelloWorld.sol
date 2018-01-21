77aa264c55191000679c2aadd0d6668ca3147593
William-Mou <10530026@sssh.tp.edu.tw>
2018-01-22 00:15:02 +0800

    my contract

--- Hello/contracts/HelloWorld.sol
+++ Hello/contracts/HelloWorld.sol
@@ -1,7 +1,53 @@
 pragma solidity ^0.4.17;
+contract smartSponsor {
+  address public owner;
+  address public benefactor;
+  bool public refunded;
+  bool public complete;
+  uint public numPledges;
+  struct Pledge {
+    uint amount;
+    address eth_address;
+    bytes32 message;
+  }
+  mapping(uint => Pledge) public pledges;
 
-contract HelloWorld {
-    function sayHello() public returns (string) {
-        return ("Hello World");
+  // constructor
+  function smartSponsor(address _benefactor) public{
+    owner = msg.sender;
+    numPledges = 0;
+    refunded = false;
+    complete = false;
+    benefactor = _benefactor;
+  }
+
+  // add a new pledge
+  function pledge(bytes32 _message)payable public {
+    if (msg.value == 0 || complete || refunded) revert();
+    pledges[numPledges] = Pledge(msg.value, msg.sender, _message);
+    numPledges++;
+  }
+
+  function getPot() constant public returns (uint){
+    return this.balance;
+  }
+
+  // refund the backers
+  function refund() public {
+    if (msg.sender != owner || complete || refunded) revert();
+    for (uint i = 0; i < numPledges; ++i) {
+      pledges[i].eth_address.transfer(pledges[i].amount);
     }
+    refunded = true;
+    complete = true;
+  }
+
+  // send funds to the contract benefactor
+  function drawdown() public {
+    if (msg.sender != owner || complete || refunded) revert();
+    benefactor.transfer(this.balance);
+    complete = true;
+  }
 }
+
+geth --datadir "./chain-1" --networkid 71 --port 30301 --nodiscover console
--- Hello/contracts/Transaction.1.sol
+++ Hello/contracts/Transaction.1.sol
@@ -3,60 +3,65 @@ pragma solidity ^0.4.17;
 contract Transaction {
     address employee;
     address employer;
-    uint balances;
     uint create_time;
     uint base_wage;
     uint payables;
     uint expire = 100;
     uint working_hours;
-    uint ETH_to_TWD_100 = 300;
+    uint ETH_to_TWD_100 = 3000000;
     bool ended = false;
     bool usaged = false;
+    /*
+    event Deposit(
+        address employee,
+        bool endeded
+    );*/
     
     function Transaction(address new_employee, uint new_working_hours ,uint new_base_wage)payable public{
-        if (ended == true) return;
+        if (ended == true) revert();
         employee = new_employee;
         employer = msg.sender;
         working_hours = new_working_hours;
         base_wage=new_base_wage;
-        balances=msg.value;
         if (working_hours >= 2) payables = (working_hours-2)*base_wage*266+working_hours*233*base_wage;
         else payables = working_hours*233*base_wage;
-        if (payables>=balances/ETH_to_TWD_100) usaged=true;
+        if (msg.value>=payables/ETH_to_TWD_100) usaged=true;
         else return;
         create_time=now;
     }
     function add_more(uint new_working_hours )payable public{
-        if (ended == true) return;
+        if (ended == true) revert();
         working_hours+=new_working_hours;
-        balances+=msg.value;
         if (working_hours >= 2) payables = (working_hours-2)*base_wage*266+working_hours*233*base_wage;
         else payables = working_hours*233*base_wage;
-        if (payables>=balances/ETH_to_TWD_100) usaged=true;
+        if (payables>=this.balance/ETH_to_TWD_100) usaged=true;
         create_time=now;
     }
-    
+    /*
     function is_expire() public returns(uint){
-        if (ended == true) return;
+        if (ended == true) revert();
         if ((now - create_time)> (expire * 1 seconds)) {
             ended = false;
-            return (now);
+            return (now - create_time);
         }
         else {
             ended = true;
-            return (now);
+            return (now - create_time);
         }
-    }
-    function test()public returns(uint){
-        return 15;
-    }
+    }*/
     
     function collectMoney() public {
-        is_expire();
-        if (msg.sender==employee && usaged == true && ended==true ) employee.transfer(balances);
-        else reserve();
+        //Deposit(employee,ended);
+        if ((now - create_time)> (expire * 1 seconds)) {
+            ended = false;
+        }
+        else {
+            ended = true;
+        }
+        if (msg.sender!=employee || ended == false || ended == false) revert();
+        employee.transfer(this.balance);
     }
 }
 
-[{"adress new_employee":"0xca35b7d915458ef540ade6068dfe2f44e8fa733c" , "uint new_working_hours": 1 , "uint new_base_wage" : 140 }]
-0xca35b7d915458ef540ade6068dfe2f44e8fa733c
+//[{"adress new_employee":"0xca35b7d915458ef540ade6068dfe2f44e8fa733c" , "uint new_working_hours": 1 , "uint new_base_wage" : 140 }]
+//0xca35b7d915458ef540ade6068dfe2f44e8fa733c
